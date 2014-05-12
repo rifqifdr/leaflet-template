4fcf2311221ddb100c467d5dd819fa75d4effad0
Roy Hewitt <royhewitt07@gmail.com>
2014-05-12 16:40:17 -0400

    initial commit

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function () {
+	createMap();
+});
+
+var createMap = function () {
+	var map = L.map('map').setView([35.302, -85.381], 6);
+
+	var Thunderforest_Landscape = L.tileLayer('http://{s}.tile.thunderforest.com/landscape/{z}/{x}/{y}.png', {
+		attribution: '&copy; <a href="http://www.opencyclemap.org">OpenCycleMap</a>, &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>'
+	}).addTo(map);
+};
